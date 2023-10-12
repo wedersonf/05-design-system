0ecd1a0cbe754c71c85d7d783c9203112505e698
wedersonf <wedymf@hotmail.com>
2023-10-12 05:56:53 -0300

    feat: docs to avatar, box, heading and text components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate consequatur distinctio qui dolore sint. Incidunt adipisci reiciendis repellat ratione doloremque porro, ipsa labore modi fugiat eum unde magnam amet excepturi.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
