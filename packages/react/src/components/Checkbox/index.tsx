7803ed4486ba1f6115c908fe78f90fbd84282de8
wedersonf <wedymf@hotmail.com>
2023-10-15 20:11:55 -0300

    feat: checkbox compoenent

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from 'react'
+import { Check } from '@phosphor-icons/react'
+import { CheckboxContainer, CheckboxIndicator } from './styled'
+
+export type CheckboxProps = ComponentProps<typeof CheckboxContainer>
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
